f6f8a36e881126b37c1122e168fe601ea4d6e79f
Patrick Chandler <chandler@localhost.localdomain>
2018-08-07 00:14:35 -0500

    Updated wrapper to use new format.

--- providers.d/git/provider_wrapper.sh
+++ providers.d/git/provider_wrapper.sh
@@ -1,6 +1,12 @@
 bashelliteProviderWrapperGit() {
   for line in $(cat ${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf); do
-    unset git_repo_alt_dir_name
+    local git_repo_alt_dir_name
+    local repo_array
+    local repo_array_size
+    local git_repo_dir_name
+    local git_repo_name
+    local git_repo_url
+    local git_sub_dirs
     IFS=$',\n'
     repo_array=( ${line} )
     unset IFS
@@ -8,23 +14,29 @@ bashelliteProviderWrapperGit() {
     if [ ${repo_array_size} == 2 ]; then
       git_repo_alt_dir_name=${repo_array[1]}
       line=${repo_array[0]}
-      git_repo_dir_name="${git_repo_alt_dir_name}"
+      git_repo_dir_name="${git_repo_alt_dir_name%.git}"
     else
       git_repo_name="${line##*/}"
-      git_repo_dir_name="${git_repo_name//.git}"
+      git_repo_dir_name="${git_repo_name%.git}"
     fi
   
-    git_repo_url="${line}"
+    git_repo_url="${line%.git}"
 
-    if [[ -d "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_repo_dir_name}" ]]; then
+    # Here we make any subdirectories that need to be made
+    git_sub_dirs="${line%/*}"
+    if [[ ! -d "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_sub_dirs}" ]]; then
+      mkdir -p "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_sub_dirs}"
+    fi
+
+    if [[ -d "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_sub_dirs}/${git_repo_dir_name}.git" ]]; then
       utilMsg INFO "$(utilTime)" "Pulling any updates from repo: ${git_repo_url}..."
-      cd "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_repo_dir_name}"
-      git pull
+      cd "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_sub_dirs}/${git_repo_dir_name}.git"
+      git fetch
     else
       utilMsg INFO "$(utilTime)" "New repo detected, cloning repo: ${git_repo_url}..."
-      cd "${_r_mirror_tld}/${_n_mirror_repo_name}"
+      cd "${_r_mirror_tld}/${_n_mirror_repo_name}/${git_sub_dirs}"
       utilMsg INFO "$(utilTime)" "Cloning to directory: ${git_repo_dir_name}..."
-      git clone "${git_repo_url}" "${git_repo_dir_name}"
+      git clone --bare "${_n_repo_url}/${git_repo_url}" "${git_repo_dir_name}.git"
     fi
   done
 }
