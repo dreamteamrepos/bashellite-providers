337f854c3ffebe5f5e6ca29d63899aec5ce24d33
Patrick Chandler <chandler@localhost.localdomain>
2018-09-11 23:46:14 -0500

    Updated bandersnatch provider.
    Updated bandersnatch provider to replace the directory parameter with
    the mirror_tld/repo_name directory being used for the sync.

--- providers.d/bandersnatch/provider_wrapper.sh
+++ providers.d/bandersnatch/provider_wrapper.sh
@@ -12,8 +12,23 @@ bashelliteProviderWrapperBandersnatch() {
 
   local directory_parameter="$(grep -oP "(?<=(^directory = )).*" ${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf)"
   if [[  "${directory_parameter}" != "${_r_mirror_tld}/${_n_repo_name}" ]]; then
-    utilMsg FAIL "$(utilTime)" "The \"directory\" parameter (${directory_parameter}) in provider.conf does not match mirror location (${_r_mirror_tld}/${_n_repo_name}); exiting."
+    utilMsg WARN "$(utilTime)" "The \"directory\" parameter (${directory_parameter}) in provider.conf does not match mirror location (${_r_mirror_tld}/${_n_repo_name})..."
   fi
+
+  local config_file="${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf"
+  local tmp_config_file="${HOME}/.bashellite/tmp_${_n_repo_name}_provider.conf"
+
+  cat /dev/null > ${tmp_config_file}
+
+  IFS=$'\n'
+  for line in $(cat ${config_file}); do
+    if [[ ${line} = *"directory = "* ]]; then
+      echo "directory = ${_r_mirror_tld}/${_n_repo_name}" >> ${tmp_config_file}
+    else
+      echo "${line}" >> ${tmp_config_file}
+    fi
+  done
+  unset IFS
 
   utilMsg INFO "$(utilTime)" "Proceeding with sync of repo (${_n_repo_name})..."
   # If dryrun is true, perform dryrun
@@ -21,7 +36,7 @@ bashelliteProviderWrapperBandersnatch() {
     utilMsg INFO "$(utilTime)" "Sync of repo (${_n_repo_name}) completed without error..."
   # If dryrun is not true, perform real run
   else
-    ${_r_providers_tld}/bandersnatch/exec/bin/bandersnatch -c "${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf" mirror;
+    ${_r_providers_tld}/bandersnatch/exec/bin/bandersnatch -c "${tmp_config_file}" mirror;
     if [[ "${?}" == "0" ]]; then
       utilMsg INFO "$(utilTime)" "Sync of repo (${_n_repo_name}) completed without error...";
     else
