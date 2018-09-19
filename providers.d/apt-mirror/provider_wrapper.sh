a1853a39906d0017e258d5e2efb07393eddb8631
Patrick Chandler <chandler@localhost.localdomain>
2018-09-19 16:18:36 -0500

    Updated to add line counter.

--- providers.d/apt-mirror/provider_wrapper.sh
+++ providers.d/apt-mirror/provider_wrapper.sh
@@ -3,7 +3,7 @@ bashelliteProviderWrapper() {
   local basepath_parameter=$(egrep "^\s*set\s*base_path\s*[[:alnum:]/_-.]*" "${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf")
   basepath_parameter="${basepath_parameter#*set*base_path}"
   if [[  "${basepath_parameter}" != "${_r_mirror_tld}/${_n_repo_name}" ]]; then
-    utilMsg WARN "$(utilTime)" "The \"base_path\" parameter (${basepath_parameter}) in provider.conf does not match mirror location (${_r_mirror_tld}/${_n_repo_name})..."
+    utilMsg WARN "$(utilTime)" "The \"base_path\" parameter ("${basepath_parameter}") in provider.conf does not match mirror location (${_r_mirror_tld}/${_n_repo_name})..."
   fi
 
   local config_file="${_r_metadata_tld}/repos.conf.d/${_n_repo_name}/provider.conf"
@@ -11,12 +11,18 @@ bashelliteProviderWrapper() {
 
   > ${tmp_config_file}
 
+  local line_counter=0
   while IFS=$'\n' read line; do
     if [[ "${line}" =~ ^set[[:blank:]]base_path[[:blank:]]+[[:alnum:]/_-.]+ ]]; then
-      echo "set base_path         ${_r_mirror_tld}/${_n_repo_name}" >> ${tmp_config_file}
+      if [ "${line_counter}" = 0 ]; then
+        echo "set base_path         ${_r_mirror_tld}/${_n_repo_name}" >> ${tmp_config_file}
+      else
+        utilMsg WARN "$(utilTime)" "Duplicate base_path parameter found.  Skipping line..."
+      fi
     else
       echo "${line}" >> ${tmp_config_file}
     fi
+    line_counter=$(($line_counter + 1))
   done < ${config_file}
 
   utilMsg INFO "$(utilTime)" "Proceeding with sync of repo (${_n_repo_name})..."
