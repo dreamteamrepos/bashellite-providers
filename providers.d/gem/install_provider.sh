e94833318cf11bf8e7bec8abf23184615a68423e
Patrick Chandler <chandler@localhost.localdomain>
2018-10-16 23:52:12 -0500

    Updated gem install_provider.sh.
    Updated gem install_provider.sh to now install ruby locally to
    bashellite's home directory.  This also installs gem.

--- providers.d/gem/install_provider.sh
+++ providers.d/gem/install_provider.sh
@@ -2,11 +2,11 @@
 
 main() {
 
-  local bin_name="gem";
+  local bin_name="ruby";
 
   for dep in \
-             ruby \
-             ${bin_name} \
+             git \
+             make \
              ; do
     which ${dep} &>/dev/null \
     || {
@@ -15,56 +15,32 @@ main() {
        };
   done;
 
-  local gemlist
+  # Check to see if ruby is already installed in /home/bashellite/.rubies, if not install it.
+  local ruby_bin="/home/bashellite/.rubies/ruby-2.5.1/bin/ruby"
+  if [[ -s "${ruby_bin}" && $(${ruby_bin} --version | grep -o "ruby 2.5.1") == "ruby 2.5.1" ]]; then
+    echo "[INFO] ${bin_name} provider successfully installed.";
+  else
+    echo "[WARN] ${bin_name} does NOT appear to be installed, (or it is broken); (re)installing..."
+    # Download ruby-install
+    git clone https://github.com/pcseanmckay/ruby-install ${providers_tld}/gem/src/ruby-install
+    
+    # Install ruby to /home/bashellite/.rubies
+    local ruby_installer="${providers_tld}/gem/src/ruby-install/bin/ruby-install"
+    local ruby_src_dir="${providers_tld}/gem/src/ruby_src"
+    local ruby_install_dir="/home/bashellite/.rubies/ruby-2.5.1"
+    ${ruby_installer} -i ${ruby_install_dir} -s ${ruby_src_dir} ruby 2.5.1
+    chown -R bashellite:bashellite /home/bashellite/.rubies
+    chmod -R 0700 /home/bashellite/.rubies
 
-  for gem in \
-      bundler \
-      rake \
-      hoe \
-      ; do
-    gemlist=$(gem list)
-    if [[ "${gemlist}" != *"${gem}"* ]]; then 
-      echo "[WARN] ${gem} gem dependency not found.  Installing..."
-      gem install ${gem}
-      gemlist=$(gem list)
-      if [[ "${gemlist}" == *"${gem}"* ]]; then
-        echo "[INFO] ${gem} installed successfully..."
-      else
-        echo "[FAIL] ${gem} was NOT installed successfully; exiting." \
-        && exit 1;
-      fi
-    fi 
-  done
-
-  gemlist=$(gem list)
-  local gem="net-http-persistent"
-  if [[ "${gemlist}" != *"${gem}"* ]]; then 
-    echo "[WARN] ${gem} gem dependency not found.  Installing..."
-    gem install -i /usr/share/gems ${gem} -v 2.9.4
-    gemlist=$(gem list)
-    if [[ "${gemlist}" == *"${gem}"* ]]; then
-      echo "[INFO] ${gem} installed successfully..."
+    # Check installation of ruby
+    if [[ -s "${ruby_bin}" && $(${ruby_bin} --version | grep -o "ruby 2.5.1") == "ruby 2.5.1" ]]; then
+      echo "[INFO] ${bin_name} provider successfully installed.";
     else
-      echo "[FAIL] ${gem} was NOT installed successfully; exiting." \
+      echo "[FAIL] ${bin_name} was NOT installed successfully; exiting." \
       && exit 1;
     fi
   fi
 
-  gemlist=$(ls ${providers_tld}/gem/exec/gems)
-  gem="rubygems-mirror"
-  if [[ "${gemlist}" != *"${gem}"* ]]; then 
-    echo "[WARN] ${gem} gem dependency not found.  Installing..."
-    gem install -i ${providers_tld}/gem/exec ${gem}
-    gemlist=$(ls ${providers_tld}/gem/exec/gems)
-    if [[ "${gemlist}" == *"${gem}"* ]]; then
-      echo "[INFO] ${gem} installed successfully..."
-    else
-      echo "[FAIL] ${gem} was NOT installed successfully; exiting." \
-      && exit 1;
-    fi
-  fi
-
-  echo "[INFO] ${bin_name} provider successfully installed.";
 }
 
 main
