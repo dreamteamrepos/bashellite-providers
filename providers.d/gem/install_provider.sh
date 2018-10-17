f80c1568075e7eba707501296a5ab7a51778fd0a
Patrick Chandler <chandler@localhost.localdomain>
2018-10-17 15:03:45 -0500

    Updated gem install_provider.sh.
    Updated gem install_provider.sh script to make ruby version a variable
    for future ruby version updating.

--- providers.d/gem/install_provider.sh
+++ providers.d/gem/install_provider.sh
@@ -3,6 +3,7 @@
 main() {
 
   local bin_name="ruby";
+  local ruby_ver="2.5.1";
 
   for dep in \
              git \
@@ -16,8 +17,8 @@ main() {
   done;
 
   # Check to see if ruby is already installed in /home/bashellite/.rubies, if not install it.
-  local ruby_bin="/home/bashellite/.rubies/ruby-2.5.1/bin/ruby"
-  if [[ -s "${ruby_bin}" && $(${ruby_bin} --version | grep -o "ruby 2.5.1") == "ruby 2.5.1" ]]; then
+  local ruby_bin="/home/bashellite/.rubies/ruby-${ruby_ver}/bin/ruby"
+  if [[ -s "${ruby_bin}" && $(${ruby_bin} --version | grep -o "ruby ${ruby_ver}") == "ruby ${ruby_ver}" ]]; then
     echo "[INFO] ${bin_name} provider successfully installed.";
   else
     echo "[WARN] ${bin_name} does NOT appear to be installed, (or it is broken); (re)installing..."
@@ -29,14 +30,14 @@ main() {
     # Install ruby to /home/bashellite/.rubies
     local ruby_installer="${providers_tld}/gem/src/ruby-install/bin/ruby-install"
     local ruby_src_dir="${providers_tld}/gem/src/ruby_src"
-    local ruby_install_dir="/home/bashellite/.rubies/ruby-2.5.1"
+    local ruby_install_dir="/home/bashellite/.rubies/ruby-${ruby_ver}"
     
-    ${ruby_installer} -i ${ruby_install_dir} -s ${ruby_src_dir} ruby 2.5.1
+    ${ruby_installer} -i ${ruby_install_dir} -s ${ruby_src_dir} ruby ${ruby_ver}
     chown -R bashellite:bashellite /home/bashellite/.rubies
     chmod -R 0700 /home/bashellite/.rubies
 
     # Check installation of ruby
-    if [[ -s "${ruby_bin}" && $(${ruby_bin} --version | grep -o "ruby 2.5.1") == "ruby 2.5.1" ]]; then
+    if [[ -s "${ruby_bin}" && $(${ruby_bin} --version | grep -o "ruby ${ruby_ver}") == "ruby ${ruby_ver}" ]]; then
       echo "[INFO] ${bin_name} provider successfully installed.";
     else
       echo "[FAIL] ${bin_name} was NOT installed successfully; exiting." \
