57ff01778dcff7a280ec051dd7b6bb4803df8d11
pcseanmckay <pcseanmckay@gmail.com>
2018-11-19 16:17:48 -0600

    Updated aptmirror provider  wrapper to correctly parse directory.

--- providers.d/apt-mirror/provider_wrapper.sh
+++ providers.d/apt-mirror/provider_wrapper.sh
@@ -13,7 +13,7 @@ bashelliteProviderWrapper() {
 
   local line_counter=0
   while IFS=$'\n' read line; do
-    if [[ "${line}" =~ ^set[[:blank:]]base_path[[:blank:]]+[[:alnum:]/_-.]+ ]]; then
+    if [[ "${line}" =~ ^set[[:blank:]]base_path[[:blank:]]+[[:alnum:]/_.-]+ ]]; then
       if [ "${line_counter}" = 0 ]; then
         echo "set base_path         ${_r_mirror_tld}/${_n_repo_name}" >> ${tmp_config_file}
       else
