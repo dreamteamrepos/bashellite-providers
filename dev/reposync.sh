b70fe091b05fbca01e91741baf5fc9a395057d00
Patrick Chandler <chandler@localhost.localdomain>
2018-12-10 16:23:27 -0600

    Updated test script.

--- dev/reposync.sh
+++ dev/reposync.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+config_file=$1
+
+while read line; do
+  tmpline=""
+  tmpline=$(echo ${line} | grep -oP "(?<=\[)[[:graph:]]+(?=\])")
+  if [[ ${tmpline} != "" ]]; then
+    echo "Found: ${tmpline}"
+  fi
+done < ${config_file};
