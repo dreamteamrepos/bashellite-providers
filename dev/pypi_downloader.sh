9098f96b2bbb6a5242d8dad73110cfe4bb91c197
pcseanmckay <pc.seanmckay@gmail.com>
2019-03-04 14:46:17 -0600

    Tested pypi package download dev script.
    Finished pypi package download dev script.

--- dev/pypi_downloader.sh
+++ dev/pypi_downloader.sh
@@ -209,19 +209,7 @@ Validate_package_framework() {
 
 # This function performs the actual sync of the package
 Sync_package() {
-  # Need to add using wget to query the pypi.org site's /simple/package/ directory to get 
-  # the index file that has the list of versions of the package.
-  # local recurse_flag="-r -l inf -np";
-  # local wget_args="${wget_args} -nv -nH -e robots=off -N"
-  # local wget_args="${wget_args} ${recurse_flag}"
-  # local wget_args="${wget_args} --accept "${wget_filename}""
-  # local wget_args="${wget_args} --reject "index*""
-  # local wget_args="${wget_args} -P "${_r_mirror_tld}/${_n_mirror_repo_name}/""
-  # local wget_args="${wget_args} "${_n_repo_url}/${wget_include_directory}${wget_filename}" "
-  # wget ${wget_args} 2>&1 \
-  # | grep -oP "(?<=(URL: ))http.*(?=(\s*200 OK$))" \
-  # | while read url; do utilMsg INFO "$(utilTime)" "Downloaded $url"; done
-  # if [[ "${PIPESTATUS[1]}" == "0" ]]; then
+  
   local pypi_url="https://pypi.org"
   local base_dir="${mirror_tld}/${mirror_package_name}/web"
   mkdir -p "${base_dir}" &>/dev/null \
@@ -252,7 +240,51 @@ Sync_package() {
 
   curl -s ${pypi_url}/simple/${mirror_package_name}/ \
   | while read line; do
-    echo "${line}" >> ${package_name_dir}/index.html
+    # Save the line to the index.html file substituting 'https://files.pythonhosted.org' with '../..' 
+    # This is to make the file downloads relative to the package being mirrored
+    # Example line:     <a href="https://files.pythonhosted.org/packages/b5/9e/ab36e384db3602fdd3729fbb3a467949c40758361f244a379b7553683663/mypy-0.1.tar.gz#sha256=0055650b0b17702e5b7d82a5b09330f9a7d500c829e9967e169bd773d538eb6b">mypy-0.1.tar.gz</a><br/>
+    local newline=${line/https\:\/\/files\.pythonhosted\.org/..\/..}
+    echo "${newline}" >> ${package_name_dir}/index.html
+
+    # Need to get the URL of the package to download
+    local package_url=`echo ${line} | grep -oP "(?<=href\=\").*(?=\"\>)"`
+    
+    # Need to get the SHA256 value to compare to the file, to see if file already exists
+    local package_sha256=`echo ${line} | grep -oP "(?<=sha256\=)[[:alnum:]]*(?=\"\>)"`
+    
+    # Need to get the package file name
+    local package_file_name=`echo ${line} | grep -oP "(?<=\"\>).*(?=\</a\>\<br/\>)"`
+
+    # Need to get the package directory
+    local package_url_dir_path=`echo ${package_url} | grep -oP "(?<=packages/).*(?=#sha256\=)"`
+    package_url_dir_path=${package_url_dir_path%/*}
+
+    mkdir -p "${packages_dir}/${package_url_dir_path}" &>/dev/null \
+      || Fail "Unable to create directory (${packages_dir}/${package_url_dir_path}); check permissions."
+
+    # Now check to see if file from package listing already exists
+    if [[ ${package_file_name} != "" ]]; then
+      local file_save="true"
+      if [ -s ${packages_dir}/$package_url_dir_path/${package_file_name} ]; then
+        local file_sha256=`sha256sum ${packages_dir}/$package_url_dir_path/${package_file_name}`
+        local file_sha256_array=( ${file_sha256} )
+
+        if [[ ${#file_sha256_array} > 0 ]]; then
+          if [[ ${file_sha256_array[0]} == ${package_sha256} ]]; then
+            file_save="false"
+          fi
+        fi
+      fi
+
+      # Save file if file_save == "true"
+      if [[ ${file_save}  == "true" ]]; then
+        echo "Saving File: ${package_file_name}..."
+        curl -s -S -o ${packages_dir}/$package_url_dir_path/${package_file_name} ${package_url}
+      else
+        echo "File: ${package_file_name} already exists, skipping..."
+      fi
+    fi
+
   done
   
 }
